1f2a14d64d7ebc67590865b9c147cebb9513dbf0
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-04 20:33:55 -0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 
 import uuid
 import redis
+from typing import Union, Callable
 """
 Create a Cache class. In the __init__ method, store an
 Type-annotate store correctly. Remember that data can be a str,
@@ -18,7 +19,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Create a store method that takes a data argument and returns a
         string. The method should generate a random key (e.g. using uuid),
@@ -28,3 +29,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fh: Callable=None):
+        """
+        create a get method that take a key string argument and an
+        optional Callable argument named fn. This callable will be
+        used to convert the data back to the desired format.
+
+        Remember to conserve the original Redis.get behavior if the
+        key does not exist.
+        """
+        if self._redis.exists(key):
+            data = self._redis.get(key)
+            if fh:
+                return fh(data)
+            return data
+        return None
+
+    def get_str(self, key: str):
+        """implement 2 new methods: get_str and get_int that will
+        automatically parametrize Cache.get with the correct
+        conversion function."""
+        return self.get(key, fh=str)
+
+    def get_int(self, key: int):
+        """return int"""
+        return self.get(key, fh=int)
