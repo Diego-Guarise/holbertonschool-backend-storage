c5d950c3f977f342011b81acb546c1903c7deece
Diego-Guarise <l.guariselopez@gmail.com>
2022-05-06 18:14:18 -0300

    ?

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,57 +1,104 @@
 #!/usr/bin/env python3
-
-import uuid
+"""
+Redis caching module
+"""
 import redis
-from typing import Union, Callable
-"""
-Create a Cache class. In the __init__ method, store an
-Type-annotate store correctly. Remember that data can be a str,
-bytes, int or float.
-"""
+from typing import Union, Callable, Optional
+import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    asd
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper"""
+        method_name = method.__qualname__
+        self._redis.incr(method_name)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    input and output list key
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper"""
+        method_name = method.__qualname__
+        data = str(args)
+        method_result = method(self, data)
+        self._redis.rpush("{}:inputs".format(method_name), data)
+        self._redis.rpush("{}:outputs".format(method_name), method_result)
+        return method_result
+    return wrapper
+
+
+def replay(func: Callable):
+    """
+    display the history
+    """
+    r = redis.Redis()
+    method_name = func.__qualname__
+    inputs = r.lrange("{}:inputs".format(method_name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(method_name), 0, -1)
+    call_number = len(inputs)
+    times_str = 'times'
+    if call_number == 1:
+        times_str = 'time'
+    msg = '{} was called {} {}:'.format(method_name, call_number, times_str)
+    print(msg)
+    for k, v in zip(inputs, outputs):
+        msg = '{}(*{}) -> {}'.format(
+            method_name,
+            k.decode('utf8'),
+            v.decode('utf8')
+        )
+        print(msg)
 
 
 class Cache:
+    """
+    class
+    """
+
     def __init__(self):
         """
-        instance of the Redis client as a private variable named _redis
-        (using redis.Redis()) and flush the instance using flushdb.
+        Initialization
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Create a store method that takes a data argument and returns a
-        string. The method should generate a random key (e.g. using uuid),
-        store the input data in Redis using the random key and return the
-        key.
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        """ returns the random key """
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
 
-    def get(self, key: str, fh: Callable=None):
+    def get(
+            self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """
-        create a get method that take a key string argument and an
-        optional Callable argument named fn. This callable will be
-        used to convert the data back to the desired format.
-
-        Remember to conserve the original Redis.get behavior if the
-        key does not exist.
+        returns the key's value
         """
-        if self._redis.exists(key):
-            data = self._redis.get(key)
-            if fh:
-                return fh(data)
-            return data
-        return None
+        result = self._redis.get(key)
+        if fn:
+            return fn(result)
+        return result
 
-    def get_str(self, key: str):
-        """implement 2 new methods: get_str and get_int that will
-        automatically parametrize Cache.get with the correct
-        conversion function."""
-        return self.get(key, fh=str)
+    def get_str(self, key) -> str:
+        """
+        returns key
+        """
+        return get(key, str)
 
-    def get_int(self, key: int):
-        """return int"""
-        return self.get(key, fh=int)
+    def get_int(self, key) -> int:
+        """
+        Returns key
+        """
+        return get(key, int)
